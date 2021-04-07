38d16b88bedcc1e7b38147fd9bf5fabd914c42af
Chih-Yu Chen <chihyu.chen1206@gmail.com>
2021-04-07 20:15:20 +0800

    Update training.csv testing.csv app.py

--- app.py
+++ app.py
@@ -22,10 +22,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     # Read the training and testing data
-    train_df = pd.read_csv(args.training)
-    test_df = pd.read_csv(args.testing)
-    train_df.columns = ("open", "high", "low", "close")
-    test_df.columns = ("open", "high", "low", "close")
+    train_df = pd.read_csv(args.training, names=("open", "high", "low", "close"))
+    test_df = pd.read_csv(args.testing, names=("open", "high", "low", "close"))
+
     # Do MinMax normalization
     maxValue = train_df.to_numpy().max()
     minValue = train_df.to_numpy().min()
@@ -35,25 +34,23 @@ if __name__ == '__main__':
     
     # Use technical analysis to expand the data 
     train["upperband"], train["middleband"], train["lowerband"] = BBANDS(train.close.to_numpy())
-    # train["ma5"] = MA(train.close.to_numpy(), timeperiod=5)
     train["sar"] = SAR(train.high.to_numpy(), train.low.to_numpy())
     train["rsi"] = RSI(train.close.to_numpy(), timeperiod=5)
     train["slowk"], train["slowd"] = STOCH(train.high.to_numpy(), train.low.to_numpy(), train.close.to_numpy())
     train_data = train.dropna()
     
     test["upperband"], test["middleband"], test["lowerband"] = BBANDS(test.close.to_numpy())
-    # train["ma5"] = MA(train.close.to_numpy(), timeperiod=5)
     test["sar"] = SAR(test.high.to_numpy(), test.low.to_numpy())
     test["rsi"] = RSI(test.close.to_numpy(), timeperiod=5)
     test["slowk"], test["slowd"] = STOCH(test.high.to_numpy(), test.low.to_numpy(), test.close.to_numpy())
 
-    # 1/0 on behalf of the open price is higher or lower after 5 days
-    train_data['week'] = np.where(train_data.open.shift(-5) > train_data.open, 1, 0)
+    # 1/0 on behalf of the open price is higher or lower after 3 days
+    train_data["threeDays"] = np.where(train_data.open.shift(-3) > train_data.open, 1, 0)
     
-    # We can't judge the correction of last five days when training 
-    train = train_data.drop(train_data.tail(5).index, inplace=False)
-    y = train.week.to_numpy()
-    X = train.drop("week", axis=1).to_numpy()
+    # We can't judge the correction of last three days when training 
+    train = train_data.drop(train_data.tail(3).index, inplace=False)
+    y = train.threeDays.to_numpy()
+    X = train.drop("threeDays", axis=1).to_numpy()
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, shuffle=False)
     
     # Use XGBClassifier and AUC to do binary classification
@@ -67,36 +64,58 @@ if __name__ == '__main__':
     # Predict the testing data
     preds = model.predict(test.values)
     ans = []
+    unit = 0
     val = 0
     # The first 8 days doesn't have Stochastic Oscillator，KD
     # So we can just predict the price based on our model
     for i in range(1, 8):
         _sum = sum(preds[i-1:i+1])
+        # bullish
         if _sum == 2:
-            val = 1 if (val == 1) else (val + 1) 
+            if unit == 1:
+                val = 0
+            else:
+                val = 1
+                unit += 1
+        # Do nothing
         elif _sum == 1:
-            val = val
+            val = 0
+        # bearish
         else:
-            val = -1 if (val == -1) else val - 1
+            if unit == -1:
+                val = 0
+            else:
+                val = -1
+                unit -= 1
+        
         ans.append(val)
     # Draw technical signs into our prediction
     for i in range(8, len(preds)):
-        _sum = sum(preds[i-1:i+1])
         isBull = (test["open"][i] > test["sar"][i], 
                   test["open"][i] >= test["middleband"][i],
-                  test["rsi"][i] < 80,
-                  test["slowk"] > test["slowd"])
-        # We are bullish that the stock price will be higher after 5 and 6 days
+                  test["rsi"][i] > 50,
+                  test["slowk"][i] >= test["slowd"][i])
+        # We are bullish that the stock price will be higher after 3 days
         # and the Technical Signs at once back the prediction 
-        if _sum == 2 or np.sum(isBull != 0) > 2:
-            val = 1 if (val == 1) else (val + 1) 
-        # We are bearish that the stock price will be higher after 5 and 6 days
+        if preds[i] == 1 and np.sum(isBull != 0) >= 2:
+            if unit == 1:
+                val = 0
+            else:
+                val = 1
+                unit += 1
+        # We are bearish that the stock price will be higher after 3 days
         # and the Technical Signs at once back the prediction 
-        elif _sum == 0 or np.sum(isBull != 0) <= 2:
-            val = -1 if (val == -1) else val - 1
+        elif preds[i] == 0 and np.sum(isBull != 0) <= 2:
+            if unit == -1:
+                val = 0
+            else:
+                val = -1
+                unit -= 1
+        # Do nothing
         else:
-            val = val
+            val = 0
         ans.append(val)
+
     # Write the result into output
     with open(args.output, "w") as fp:
         for i in range(len(ans)):
