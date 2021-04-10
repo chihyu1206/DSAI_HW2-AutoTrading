e2bfcb7c485528d0ea603e9ab655f7cff39c4a90
Chih-Yu Chen <chihyu.chen1206@gmail.com>
2021-04-10 13:30:36 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzvf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+make install
+python3 -m pip3 install Ta-Lib
+git clone --recursive http://github.com/dmlc/xgboost
+cd xgboost
+make -j4
+cd python-package
+python3 setup.py install                   
